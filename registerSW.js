5562b0e9f4f2a91baa20a6c8884b70abd6fdcb26
Prem Pathak <pathakpremkumar95@gmail.com>
2025-06-27 02:08:49 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/currency-converter/sw.js', { scope: '/currency-converter/' })})}
